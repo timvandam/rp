b1e0b96818e8c84a714690e474ebf7101c5cd2d5
Tim van Dam <timvandamcs@gmail.com>
2022-05-12 17:22:42 +0200

    masking based on the TS AST - It now looks for a leaf in the AST and masks it by wrapping it in comments. This leads to the token also being wrapped in this comment in the JS version, making it possible to have equivalent masks in both casess - TODO: Compiling to JS. This is very convoluted with  opposed to with  but the latter will not add comments (probably a bug) - TODO: Better selection of what to mask: only leafs? entire objects? only a single object property?

--- aaa.ts
+++ aaa.ts
@@ -1,22 +1,62 @@
-import * as tsc from 'typescript'
-import { ScriptTarget, SyntaxKind } from 'typescript'
-import { readFileSync } from 'fs'
+import { Node, Project, SyntaxKind, ts } from 'ts-morph'
 
-const sf = tsc.createSourceFile('x.temp', readFileSync('./test2.ts', 'utf8'), ScriptTarget.ESNext, true)
+// const sf = tsc.createSourceFile('x.temp', readFileSync('./test2.ts', 'utf8'), ScriptTarget.ESNext, true)
+//
+// const CONST = sf.statements[0]
+// // console.log(sf.statements[0].getChildren(sf).map(x=>[SyntaxKind[x.kind],x.parent?SyntaxKind[x.parent.kind]:-1,x.parent]))
+// // console.log(CONST.parent)
+// // console.log(CONST.getChildren()[0].getChildren()[1].getChildren()[0].getChildren()[4].getChildren().map(x => x.getFullText()))
+// // const ONE = CONST.getChildren()[0].getChildren()[1].getChildren()[0].getChildren()[4].getChildren()[0]
+// // tsc.addSyntheticLeadingComment(ONE, SyntaxKind.MultiLineCommentTrivia, 'hello world', false)
+// // tsc.addSyntheticTrailingComment(ONE, SyntaxKind.MultiLineCommentTrivia, 'bye world', false)
+// // tsc.addSyntheticLeadingComment(CONST.parent, SyntaxKind.MultiLineCommentTrivia, 'yeet', false)
+//
+// function transformSourceFile(sf: SourceFile, fn: (node: Node) => void) {
+// 	function transformNode(node: Node) {
+// 		fn(node)
+// 		node.forEachChild(transformNode)
+// 	}
+// 	sf.forEachChild(transformNode)
+// 	for (const child of sf.getChildren()) transformNode(child)
+// }
+//
+// tsc.transform(sf, [context => sourceFile => {
+// 	transformSourceFile(sourceFile, node => {
+// 		if (node.getChildCount() === 0) {
+// 			tsc.addSyntheticLeadingComment(node, SyntaxKind.MultiLineCommentTrivia, 'hello world', false)
+// 			tsc.addSyntheticTrailingComment(node, SyntaxKind.MultiLineCommentTrivia, 'bye world', false)
+// 		}
+// 	})
+// 	return sourceFile
+// }])
+//
+// const printer = tsc.createPrinter({removeComments:false})
+// console.log(printer.printFile(sf))
+// console.log('---------------------')
 
-const CONST = sf.statements[0]
-// console.log(sf.statements[0].getChildren(sf).map(x=>[SyntaxKind[x.kind],x.parent?SyntaxKind[x.parent.kind]:-1,x.parent]))
-// console.log(CONST.parent)
-console.log(CONST.getChildren()[0].getChildren()[1].getChildren()[0].getChildren()[4].getChildren().map(x => x.getFullText()))
-const ONE = CONST.getChildren()[0].getChildren()[1].getChildren()[0].getChildren()[4].getChildren()[0]
-tsc.addSyntheticLeadingComment(ONE, SyntaxKind.MultiLineCommentTrivia, 'hello world', false)
-tsc.addSyntheticTrailingComment(ONE, SyntaxKind.MultiLineCommentTrivia, 'bye world', false)
-// tsc.addSyntheticLeadingComment(CONST.parent, SyntaxKind.MultiLineCommentTrivia, 'yeet', false)
-// @ts-ignore
+const p = new Project({ compilerOptions: { removeComments: false } })
+const psf = p.createSourceFile('x.ts', `const x: number = 5 * 9;`)
 
-console.log('---------------------')
-const printer = tsc.createPrinter({removeComments:false})
-console.log(printer.printFile(sf))
+function transformRecursively(node: Node, fn: (node: Node) => void): void {
+	fn(node)
+	node.forEachDescendant(fn)
+}
+
+psf.getStatements().forEach(x => transformRecursively(x, node => {
+	ts.addSyntheticLeadingComment(node.compilerNode, SyntaxKind.MultiLineCommentTrivia, 'A', false)
+}))
+
+
+const x = psf.transform(traversal => {
+	const node = traversal.visitChildren()
+	if (node.getChildCount() === 0) {
+		return ts.addSyntheticLeadingComment(node, SyntaxKind.MultiLineCommentTrivia, 'A', false)
+	}
+	return node
+})
+console.log(x.print({removeComments:false})) // no comments!
+console.log(psf.print({removeComments:false})) // no comments!
+
 
 /*
 const sf = new Project().createSourceFile('x', readFileSync('./test2.ts', 'utf8'))
--- src/masking/mask.ts
+++ src/masking/mask.ts
@@ -1,4 +1,5 @@
-import { Project, ScriptTarget } from 'ts-morph'
+import * as tsc from 'typescript'
+import { Node, ScriptTarget, SyntaxKind } from 'typescript'
 
 // n is 0 indexed
 function replaceNthOccurrence(str: string, keyword: string, n: number, replacement: string = ''): string {
@@ -6,26 +7,57 @@ function replaceNthOccurrence(str: string, keyword: string, n: number, replaceme
 	return str.replace(new RegExp(keyword, 'g'), match => i++ === n ? replacement : match)
 }
 
-export function maskCode(code: string): { ts: string; js: string; } {
-	const project = new Project({ compilerOptions: { target: ScriptTarget.ESNext } })
-	const sourceFile = project.createSourceFile('temp.ts', code)
-	const [fn] = sourceFile.getFunctions()
+function reduceNode<T>(node: Node, fn: (acc: T, node: Node) => T, initial: T): T {
+	let cur = fn(initial, node)
+	node.forEachChild(child => {
+		cur = reduceNode(child, fn, cur)
+	})
+	return cur
+}
 
-	const body = fn.getBody()?.print({ removeComments: true })
+export function maskSingleToken(code: string): { ts: string; js: string; } {
+	const sourceFile = tsc.createSourceFile('temp.ts', code, ScriptTarget.ESNext, true)
+	const [fn] = sourceFile.statements.filter(tsc.isFunctionDeclaration)
+
+	const body = fn.body
 	if (body === undefined) throw new Error('No function body available')
 
-	//TODO: Better token selection
-	const tokens = body.split(/[.\s;(){}*^%/\-+=]/).filter(x => x.length > 0)
-	const tokenIndex = Math.floor(Math.random() * tokens.length)
-	const maskedToken = tokens[tokenIndex]
-	const tokenPrevCount = tokens.slice(0, tokenIndex).reduce((count, token) => count + (token === maskedToken ? 1 : 0), 0)
-
-	fn.setBodyText(replaceNthOccurrence(body, maskedToken, tokenPrevCount, `/* <mask> */ ${maskedToken} /* </mask> */`))
+	// TODO: Not just leafs
+	const leafCount = reduceNode(body, (t, n) => t + (n.getChildCount() === 0 ? 1 : 0), 0)
+	const leafToMask = Math.floor(Math.random() * leafCount)
+	//TODO: Exit sooner
+	reduceNode(body, (r, n) => {
+		if (n.getChildCount() !== 0) return r
+		if (r === 0) {
+			// We are at the token that will be masked
+			tsc.addSyntheticLeadingComment(n, SyntaxKind.MultiLineCommentTrivia, '<mask>', false)
+			tsc.addSyntheticTrailingComment(n, SyntaxKind.MultiLineCommentTrivia, '</mask>', false)
+		}
+		return r - 1
+	}, leafToMask)
 
 	return {
-		ts: sourceFile.getFullText(),
-		js: sourceFile.getEmitOutput().getOutputFiles()[0].getText()
+		ts: tsc.createPrinter({ removeComments: false }).printFile(sourceFile),
+		js: 'asd' // TODO
 	}
 }
 
 
+const res = maskSingleToken(`/**
+ * Factory for \`CacheableGroup\` objects.
+ *
+ * @export
+ * @param {Group} group - group to be cached
+ * @returns {CacheableGroup | undefined}
+ */
+export function cacheableGroupFactory(group: Group): CacheableGroup | undefined {
+    const now = new Date().getTime();
+    if (group) {
+        return { group, timestamp: now };
+    }
+    else {
+        return undefined;
+    }
+}
+`)
+console.log(`${res.ts}\n-------------------------\n${res.js}`)
